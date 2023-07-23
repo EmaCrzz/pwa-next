4481271d7fd8accb6f72235c00fb8b7f174fcbe6
EmaCrzz <ema_villanueva@hotmail.com>
2023-07-22 21:45:40 -0300

    add initial config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/favicon.ico"></link>
+        <meta name="theme-color" content="#000" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
